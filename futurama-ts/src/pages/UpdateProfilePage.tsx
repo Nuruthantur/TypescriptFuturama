65f4e03967b40ad95eab4fe59c3fa05ce99ffe1a
Nuruthantur <olivermayer123@gmail.com>
2024-04-12 20:43:36 +0200

    "Add input fields for updating user profile information"
    This commit adds several new input fields to the UpdateProfilePage component, allowing users to update various aspects of their profile such as username, first name, last name, birth date, gender,

--- futurama-ts/src/pages/UpdateProfilePage.tsx
+++ futurama-ts/src/pages/UpdateProfilePage.tsx
@@ -2,10 +2,29 @@ import { useContext, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
 
 function UpdateProfilePage() {
-  //   const { loading } = useContext(AuthContext);
+  const { loading } = useContext(AuthContext);
 
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [birthDcate, setBirthDate] = useState("");
+  const [gender, setGender] = useState("");
+  const [bio, setBio] = useState("");
+  const [location, setLocation] = useState("");
+
+  // const [image, setImage] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [confirmPassword, setConfirmPassword] = useState("");
+  // userName
+  // firstName
+  // lastName
+  // birthDate
+  // aboutYourSelf
+  // travelingDates
+  // travelingDestinations
+  // hobbies
+  // userPicture
 
   return (
     <form
@@ -22,10 +41,46 @@ function UpdateProfilePage() {
         onChange={(e) => setEmail(e.target.value)}
       />
       <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
+        type="text"
+        name="username"
+        value={username}
+        onChange={(e) => console.log(e)}
+      />
+      <input
+        type="text"
+        name="firstName"
+        value={firstName}
+        onChange={(e) => console.log(e)}
+      />
+      <input
+        type="text"
+        name="lastName"
+        value={lastName}
+        onChange={(e) => console.log(e)}
+      />
+      <input
+        type="text"
+        name="birthDcate"
+        value={birthDcate}
+        onChange={(e) => console.log(e)}
+      />
+      <input
+        type="text"
+        name="gender"
+        value={gender}
+        onChange={(e) => console.log(e)}
+      />
+      <input
+        type="text"
+        name="bio"
+        value={bio}
+        onChange={(e) => console.log(e)}
+      />
+      <input
+        type="text"
+        name="location"
+        value={location}
+        onChange={(e) => console.log(e)}
       />
       <button type="submit">Update</button>
       {/* <button type="submit">{loading ? "Loading..." : submit}</button> */}
